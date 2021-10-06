7bd5164400bec9d73a710c1b76c84a51298e8a11
Rafael Benevides <rafabene@gmail.com>
2021-10-06 11:27:59 -0300

    Changed IP do minikube

--- scripts/deploy/run-session-add.sh
+++ scripts/deploy/run-session-add.sh
@@ -7,7 +7,7 @@ then
     echo "Inform the value to add to session"
     exit 1
 else
-    url="192.168.100.100:$INGRESS_PORT/session/add/$value"
+    url="http://`minikube ip`:$INGRESS_PORT/session/add/$value"
 fi
 
 curl -b cookie.txt -c cookie.txt $url
--- scripts/deploy/run-session-get.sh
+++ scripts/deploy/run-session-get.sh
@@ -2,7 +2,7 @@
 export INGRESS_PORT=$(kubectl -n microservices get service ola -o jsonpath='{.spec.ports[?(@.name=="http-3000")].nodePort}')
 value=$1
 
-    url="http://192.168.100.100:$INGRESS_PORT/session/get"
+    url="http://`minikube ip`:$INGRESS_PORT/session/get"
 
 while true
 do curl -b cookie.txt -c cookie.txt $url
